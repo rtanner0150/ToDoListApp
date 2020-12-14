c178fd69be8cd1ddfce9083098fb6de7425a6612
rtanner0150 <71511920+tannertronic@users.noreply.github.com>
2020-12-14 15:59:01 -0600

    oh man
    i should have committed sooner

--- public/script.js
+++ public/script.js
@@ -9,14 +9,81 @@ async function getToDoList(){
     if (response.status != 200){
         throw Error(body.message);
     }
-    return body;   
+    return body;
 }
 
-function clickButton(){
-    getToDoList().then((body) => {
-        let objs = JSON.stringify(body);
-        document.body.append(objs);
-    }).catch((err) => {
-        console.log(err);
-    });
+async function getSingleTask(id){
+    let requestOptions = {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+    }
+
+    const response = await fetch('/tasks/' + id, requestOptions);
+    const body = await response.json();
+    if (response.status != 200){
+        throw Error('Error!');
+    }
+    return body;
+}
+
+async function addTask(){
+    let prioritySelect = document.getElementById('priority');
+    let completedSelect = document.getElementById('completed');
+    let t = {
+        name: document.getElementById('name').value,
+        assignedTo: document.getElementById('assignee').value,
+        priority: prioritySelect.options[prioritySelect.selectedIndex].value,
+        completed: completedSelect.options[completedSelect.selectedIndex].value
+    }
+    let requestOptions = {
+        method: 'POST',
+        body: JSON.stringify(t),
+        headers: {'Content-Type': 'application/json'}
+    }
+
+    const response = await fetch('/tasks', requestOptions);
+    if (response.status != 200){
+        throw Error('task not saved!');
+    }
+    window.location.href = 'index.html';
+    return true;
+}
+
+async function deleteTask(el){
+    let requestOptions = {
+        method: 'DELETE', 
+        headers: {'Content-Type': 'application/json'}
+    }
+    let deleteId = el.closest('.task').dataset.id;
+
+    const response = await fetch('/tasks/' + deleteId, requestOptions);
+    if (response.status != 204){
+        throw Error('task not deleted');
+    }
+    window.location.href = 'index.html';
+    return true;
+}
+
+async function updateTask(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    let prioritySelect = document.getElementById('priority');
+    let completedSelect = document.getElementById('completed');
+    let t = {
+        name: document.getElementById('name').value,
+        assignedTo: document.getElementById('assignee').value,
+        priority: prioritySelect.options[prioritySelect.selectedIndex].value,
+        completed: completedSelect.options[completedSelect.selectedIndex].value
+    }
+    let requestOptions = {
+        method: 'PUT',
+        body: JSON.stringify(t),
+        headers: {'Content-Type': 'application/json'}
+    }
+    const response = await fetch('/tasks/' + id, requestOptions);
+    if (response.status != 200){
+        throw Error('task not saved!');
+    }
+    window.location.href = 'index.html';
+    return true;
 }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const bodyParser = require('body-parser');
 
 let Task = require('./models/Task.js');
 let List = require('./models/List.js');
@@ -18,6 +19,8 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.listen(port, () => {
     console.log('The Express server is running at port ' + port);
@@ -37,6 +40,35 @@ app.get('/tasks/:id', (request, response) => {
     })
 });
 
+app.post('/tasks', (request, response) => {
+    let t = new Task(request.body);
+    t.save((err, t) => {
+        if (err){
+            response.sendStatus(500);
+            return console.error(err);
+        }
+        response.sendStatus(200);
+    })
+});
+
+app.delete('/tasks/:id', async (request, response) => {
+    try {
+        await Task.deleteOne({_id: request.params.id});
+        response.sendStatus(204);
+    } catch {
+        response.sendStatus(404);
+    }
+});
+
+app.put('/tasks/:id', async (request, response) => {
+    let updatedTask = new Task(request.body);
+    Task.findOne({_id: request.params.id}).exec((err, item) => {
+        if (err) return console.error(err);
+        item = updatedTask;
+        item.save();
+    });
+});
+
 app.get('/search/:prop/:val', (request, response) => {
     let propParam = request.params.prop;
     let valParam = request.params.val;
@@ -47,67 +79,3 @@ app.get('/search/:prop/:val', (request, response) => {
         response.send(items);
     })
 });
-
-// app.delete('/tasks/:id', (request, response) => {
-//     Task.findOneAndDelete({_id: request.params.id}).exec((err, item) => {
-
-//     });
-// });
-
-// db.once('open', function(){
-//     console.log('mongoose connected');
-
-    // const poopSchema = new mongoose.Schema({
-    //     name: String,
-    //     number: Number
-    // });
-    // const Poop = mongoose.model('Poop', poopSchema);
-
-    // let task1 = new Task({
-    //     name        : 'Do Laundry',
-    //     priority    : 'Medium',
-    //     assignedTo  : 'Ryan',
-    //     completed   : true
-    // });
-    // task1.save(function(err){
-    //     if (err) console.errors(err);
-    //     console.log('task saved');
-    // });
-
-    // let myList = new List({
-    //     name    : 'Ryan\'s List'
-    // });
-    // myList.tasks.push(task1);
-    // myList.save(function(err, list){
-    //     if (err) console.errors(err);
-    //     console.log('list saved');
-    // });
-
-    // let myList = List.findOneAndDelete().exec(function(err, list){
-    //     if (err) return console.errors(err);
-    //     console.log('list deleted');
-    // })
-
-    /* getting populate() to work:
-        problem was that i was creating the task locally, adding it to the list, and saving the list to the DB
-        but i wasn't saving the task to the DB, so the list had a refId to an object that did not exist
-        making sure the task is saved to the DB as well allows me to populate() by id correctly 
-        must remember */
-    // let myList = List.findOne().populate('tasks').exec(function(err, list){
-    //     if (err) return console.errors(err);
-    //     //console.log(list);
-    //     Task.find({name: 'Add some data'}).then(function(task){
-    //         if (err) return console.errors(err);
-    //         list.tasks.push(task);
-    //     }); 
-        
-    // });
-
-    // let myTask = Task.find({name: 'Add some data'}).exec(function(err, task){
-    //     if (err) return console.errors(err);
-    //     console.log(task);
-    // });
-    // console.log(myList);
-
-    //Task.findOneAndUpdate({name: 'Do Laundry'}, {priority: 'Low'}, function(){console.log('item updated?')});
-// });
